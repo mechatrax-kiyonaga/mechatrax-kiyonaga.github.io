ff6bc78deccbe8ecc2b6da74c262f0b355aa42c8
mechatrax-kiyonaga <kiyonaga@mechatrax.com>
2021-12-07 10:13:44 +0000

    update

--- setup/raspbian-11.sh
+++ setup/raspbian-11.sh
@@ -5,7 +5,8 @@ set -eu
 REPO=http://mechatrax-kiyonaga.github.io/
 DIST=bullseye
 DEB=mechatrax-archive-keyring_2016.12.19.3_all.deb
-SHA256=de828a5a50bd53369830d603f18f777e5fcbcd0830b1f714982a0da53ebbc6e6
+#SHA256=de828a5a50bd53369830d603f18f777e5fcbcd0830b1f714982a0da53ebbc6e6
+SHA256=d7aa68b15ae62e6aaf48064ddc8732bfb8dcae10e8a077686a7803f6cb952120
 TMPDIR=$(mktemp -d /tmp/io.github.mechatrax.raspbian.${DIST}.setup.XXXXXXX)
 
 pushd $TMPDIR

ecbc161d95a6c4bc74dd5acb1ac9a6ab9c0155b4
mechatrax-kiyonaga <kiyonaga@mechatrax.com>
2021-12-08 02:47:49 +0000

    update

--- setup/raspbian-11.sh
+++ setup/raspbian-11.sh
@@ -8,17 +8,19 @@ DEB=mechatrax-archive-keyring_2016.12.19.3_all.deb
 #SHA256=de828a5a50bd53369830d603f18f777e5fcbcd0830b1f714982a0da53ebbc6e6
 SHA256=d7aa68b15ae62e6aaf48064ddc8732bfb8dcae10e8a077686a7803f6cb952120
 TMPDIR=$(mktemp -d /tmp/io.github.mechatrax.raspbian.${DIST}.setup.XXXXXXX)
+KEY=test-archive.gpg
 
 pushd $TMPDIR
 echo "$SHA256 $DEB" > sha256.txt
 wget http://mechatrax-kiyonaga.github.io/pool/main/m/mechatrax-archive-keyring/$DEB
+cp ./mechatrax-archive-keyring_2016.12.19.3_all.deb /usr/share/keyrings/
 #sha256sum -c sha256.txt
 dpkg -i $DEB
 popd
 rm -rf $TMPDIR
 
 cat << EOF > /etc/apt/sources.list.d/mechatrax.list
-deb $REPO $DIST main contrib non-free
+deb [signed-by=/usr/share/keyrings/mechatrax-archive-keyring_2016.12.19.3_all.deb] $REPO $DIST main contrib non-free
 # Uncomment line below then 'apt-get update' to enable 'apt-get source'
 #deb-src $REPO $DIST main contrib non-free
 EOF
